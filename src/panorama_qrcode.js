682a12d79995c19b96a2550e09f50324ad03ca8e
Xavier Zheng <Xavier@dota2x.cn>
2022-09-06 22:34:22 +0800

    optimize the qr code generation

--- src/panorama_qrcode.js
+++ src/panorama_qrcode.js
@@ -1,21 +1,27 @@
-function CreateQRCode(data, container) {
-    container.RemoveAndDeleteChildren();
-    container.style.flowChildren = "down";
-    let qrcode = new QRCode(-1, 0);
-    qrcode.addData(data);
-    qrcode.make();
-    let size = qrcode.getModuleCount();
-    let pix_size = Math.floor(container.actuallayoutwidth / size);
-    for (let row = 0; row < size; ++row) {
-        let row_container = $.CreatePanel("Panel", container, "");
-        row_container.style.flowChildren = "right";
-        for (let col = 0; col < size; ++col) {
-            let pix = $.CreatePanel("Panel", row_container, "");
-            pix.style.width = pix_size + "px";
-            pix.style.height = pix_size + "px";
-            pix.style.backgroundColor = qrcode.isDark(row, col)
-                ? "#000000"
-                : "#ffffff";
-        }
-    }
+function CreateQRCode(data, container, width) {
+	container.RemoveAndDeleteChildren();
+	container.style.flowChildren = "none";
+	let qrcode = new QRCode(-1, 0);
+	qrcode.addData(data);
+	qrcode.make();
+	let size = qrcode.getModuleCount();
+	let pix_size = width / size;
+	container.style.backgroundColor = '#fff';
+	for (let row = 0; row < size; ++row) {
+		for (let col = 0; col < size; ++col) {
+			if (!qrcode.isDark(row, col)) continue;
+			let pix = $.CreatePanel("Panel", container, "");
+			pix.style.marginLeft = col * pix_size + `px`;
+			pix.style.marginTop = row * pix_size + `px`;
+			let rect_width = pix_size;
+			while (col + 1 < size && qrcode.isDark(row, col + 1)) {
+				rect_width += pix_size;
+				col++;
+			}
+			pix.style.width = rect_width + "px";
+			pix.style.height = pix_size + "px";
+			pix.style.backgroundColor = "#000";
+		}
+	}
 }
+
